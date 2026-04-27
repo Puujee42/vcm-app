df706df24de7a5200f4c1e6a57d284c9275d3fd4
Puujee42 <kayanokouji22@gmail.com>
2026-04-27 13:59:11 +0800

    fix

--- app/components/BannerSlider.tsx
+++ app/components/BannerSlider.tsx
@@ -13,14 +13,14 @@
    {
      id: 'fallback-1',
      title: 'Inspiration in Action',
-     image: 'https://scontent.fuln2-2.fna.fbcdn.net/v/t39.30808-6/574080514_1254673616704746_2734634584703551112_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=b895b5&_nc_ohc=uYLUQuf89tsQ7kNvwGYoJEF&_nc_oc=Adr5B5mRR_G_oHJ4YZFJ8Oj3sprfLF43RPikOwJL6QbS3I9q22yWgOsYoIkgPns8jdA&_nc_zt=23&_nc_ht=scontent.fuln2-2.fna&_nc_gid=7jGdMiJroBpPsgBPaSM5sA&_nc_ss=7a3a8&oh=00_Af06quI8sJ5jVTAAJTpvavP1wWO29hw7FXUT2bph8ZfM0g&oe=69E5073D',
+     image: 'https://images.unsplash.com/photo-1542744173-8e7e53415bb0?auto=format&fit=crop&q=80&w=1600',
      link: '/about',
      active: true,
    },
    {
      id: 'fallback-2',
      title: 'Shoebox Project Mongolia',
-     image: 'https://scontent.fuln8-1.fna.fbcdn.net/v/t39.30808-6/617134620_1318655110306596_6087394076990716216_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=105&ccb=1-7&_nc_sid=b895b5&_nc_ohc=0Teggzw7IzIQ7kNvwF3Gki7&_nc_oc=AdoBmfXYXrs5HrVSWILuIEEZpXG7siHhksp0GK7O6Faj-ORep2wqv2fBJqEs50_68ms&_nc_zt=23&_nc_ht=scontent.fuln8-1.fna&_nc_gid=eAn8Gar149Kvu1K4ldrS5w&_nc_ss=7a3a8&oh=00_Af2OiwWGy3wRlAOIZB0HlLhiJM_LqwNFVD3OYxAX16cmdw&oe=69E525DF',
+     image: 'https://images.unsplash.com/photo-1511632765486-a01980e01a18?auto=format&fit=crop&q=80&w=1600',
      link: '/events',
      active: true,
    },
